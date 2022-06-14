87cd0f7dcdbdbf3040364da32f605788262253f4
Avid <aldiyar.ismailov@gmail.com>
2022-06-14 14:40:49 +0600

    Added date and icon

--- src/header/index.js
+++ src/header/index.js
@@ -1,10 +1,16 @@
 import "./header.css";
+// import MyIcon from "../media/moon-svgrepo-com.svg";
+import { ReactComponent as IconMenu } from "../media/moon-svgrepo-com.svg";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
     <>
       <div className="header-wrapper">
-        <div className="header-row"></div>
+        <div className="header-row">
+          <IconMenu className="icon" />
+          <div>{format(new Date(), "d.MM.y  HH:mm")}</div>
+        </div>
       </div>
     </>
   );
